8d59785dfff3683f7a5dce9291fa44364a7e2689
BraulioFer <brauliomelofer@gmail.com>
2023-05-05 16:57:56 +0100

    Exercicio do Hotel

---
+++ Exercicio Hotel/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Quantos quartos vai alugar? ");
+        Console.WriteLine();
+
+        int q = int.Parse(Console.ReadLine());
+    }
+}
